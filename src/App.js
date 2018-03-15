40c3f69af6e526a99e6a49059a787d9b0866b4b2
Randy Tsao <randytsao24@gmail.com>
2018-03-15 17:20:03 -0400

    Added header to app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
-import Button from 'antd/lib/button';
+import { Layout, Menu, Breadcrumb, Button } from 'antd';
 import './App.css';
+
+const { Header, Content, Footer } = Layout;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Four on Four</h1>
-        </header>
-        <Button type="primary">TESTING</Button>
-      </div>
+      <Layout className="layout">
+        <Header className='app-header'>
+          <div className="app-title">Four on Four</div>
+          <Menu
+            className='header-menu'
+            mode="horizontal"
+            defaultSelectedKeys={['1']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1">Sequencer</Menu.Item>
+            <Menu.Item key="2">Login</Menu.Item>
+            <Menu.Item key="3">Sign Up</Menu.Item>
+          </Menu>
+        </Header>
+      </Layout>
     );
   }
 }
