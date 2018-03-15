da3337989b76ee7c206bef6f8b65426ca39bc3c0
Randy Tsao <randytsao24@gmail.com>
2018-03-15 16:41:30 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Button from 'antd/lib/button';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Four on Four</h1>
+        </header>
+        <Button type="primary">TESTING</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
